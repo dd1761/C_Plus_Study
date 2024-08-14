69c50ccbaf19700def708c8deb7665477df70674
KIMDONGBEEN <dd1761@naver.com>
2024-08-14 12:13:57 +0900

    0814

--- 0814_1.cpp
+++ 0814_1.cpp
@@ -1,8 +1,15 @@
-#include <iostream>
+#include <iostream> //전처리 지시자
+
+/*
+c++에서 함수를 사용하고자 한다면
+반드시 그 함수의 원형을 미리 정의해야 함.
+*/
 
 using namespace std;
 
-int main() {
+int main_1() {
+	// c++ 코드에는 반드시
+	// main의 이름을 가지고 있는 함수가 있어야 한다.
 
 	cout << "Hello World!" << endl;
 
